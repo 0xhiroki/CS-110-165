61d2fddabe14e24338ca68b6d355a7d1395fee79
Hiroki Hori <hirokihori0814@gmail.com>
2015-09-11 18:59:56 -0700

    Refactor structure-lab.cpp

--- COMSC-165/Lab-2015-09-12/structure-lab.cpp
+++ COMSC-165/Lab-2015-09-12/structure-lab.cpp
@@ -37,7 +37,7 @@ struct GraphicCard {
     BaseInfo base;
 };
 
-struct HDD{
+struct HDD {
     BaseInfo base;
     int capacity;
     string interface;
@@ -64,46 +64,43 @@ struct PC {
     Monitor monitor;
 };
 
-int searchPC(int num, string keyword, PC pcs[NUM_OF_PC], bool& isFound) {
+int searchPC(int menuNumber, string keyword, const vector<PC> &pcs) {
+    int result = -1;
+    switch (menuNumber-1) {
+        case 0:
+            for (int i=0; i<NUM_OF_PC && result==-1; i++) {
+                if (pcs.at(i).base.name == keyword) {
+                    result = i;
+                }
+            }
+                break;
+        case 1:
+            for (int i=0; i<NUM_OF_PC && result==-1; i++) {
+                if (pcs.at(i).base.serialNum == keyword) {
+                    result = i;
+                }
+            }
+                break;
+        case 2:
+            for (int i=0; i<NUM_OF_PC && result==-1; i++) {
+                if (pcs.at(i).base.brand == keyword) {
+                    result = i;
+                }
+            }
+                break;
 
-    int result;
-    switch (num) {
-        case 1: for (int i=0; i<NUM_OF_PC; i++) {
-                    if (pcs[i].base.name == keyword) {
-                        result = i;
-                        isFound = true;
-                    }
+        case 3:
+            for (int i=0; i<NUM_OF_PC && result==-1; i++) {
+                if (pcs.at(i).base.model == keyword) {
+                    result = i;
                 }
-                break;
-        case 2: for (int i=0; i<NUM_OF_PC; i++) {
-                    if (pcs[i].base.serialNum == keyword) {
-                        result = i;
-                        isFound = true;
-                    }
-                }
-                break;
-        case 3: for (int i=0; i<NUM_OF_PC; i++) {
-                    if (pcs[i].base.brand == keyword) {
-                        result = i;
-                        isFound = true;
-                    }
-                }
-                break;
-
-        case 4: for (int i=0; i<NUM_OF_PC; i++) {
-                    if (pcs[i].base.model == keyword) {
-                        result = i;
-                        isFound = true;
-                    }
-                }
-                break;
+            }
+            break;
     }
-
     return result;
-
 }
 
-int chooseMenu() {
+int getMenuNumberFromUser() {
     string transfer;
     int num;
     cout << "Which element do you want to use?\n"
@@ -117,61 +114,63 @@ int chooseMenu() {
     return num;
 }
 
-
-string getInput() {
+string getKeywordFromUser() {
     string keyword;
     cout << "Input the keyword: ";
     getline(cin, keyword);
     return keyword;
 }
 
-void displayResult(int i, PC pcs[NUM_OF_PC]) {
+void displayResult(int selectedIndex, vector<PC> pcs) {
+    int i = selectedIndex;
+    cout << endl;
     cout << "General info-------------------------------\n"
-         << "computer name: " << pcs[i].base.name << endl
-         << "brand: " << pcs[i].base.brand << endl
-         << "model: " << pcs[i].base.model << endl
-         << "serial num: " << pcs[i].base.serialNum << endl;
+         << "computer name: " << pcs.at(i).base.name << endl
+         << "brand: " << pcs.at(i).base.brand << endl
+         << "model: " << pcs.at(i).base.model << endl
+         << "serial num: " << pcs.at(i).base.serialNum << endl;
 
     cout << "CPU info-----------------------------------\n"
-         << "# of core: " << pcs[i].cpu.numberOfcore << endl
-         << "speed: " << pcs[i].cpu.speed << endl
-         << "serial num: " << pcs[i].cpu.base.serialNum << endl;
+         << "# of core: " << pcs.at(i).cpu.numberOfcore << endl
+         << "speed: " << pcs.at(i).cpu.speed << endl
+         << "serial num: " << pcs.at(i).cpu.base.serialNum << endl;
 
     cout << "Graphics card------------------------------\n"
-         << "brand: " << pcs[i].graphicCard.base.brand << endl
-         << "model: " << pcs[i].graphicCard.base.model << endl
-         << "serial num: " <<pcs[i].graphicCard.base.serialNum << endl;
+         << "brand: " << pcs.at(i).graphicCard.base.brand << endl
+         << "model: " << pcs.at(i).graphicCard.base.model << endl
+         << "serial num: " <<pcs.at(i).graphicCard.base.serialNum << endl;
 
     cout << "HDD----------------------------------------\n"
-         << "capacity: " << pcs[i].hdd.capacity << endl
-         << "interface: " << pcs[i].hdd.interface << endl
-         << "serial num: " << pcs[i].hdd.base.serialNum << endl;
+         << "capacity: " << pcs.at(i).hdd.capacity << endl
+         << "interface: " << pcs.at(i).hdd.interface << endl
+         << "serial num: " << pcs.at(i).hdd.base.serialNum << endl;
 
     cout << "Monitors-----------------------------------\n"
-         << "height: " << pcs[i].monitor.screenSize.height << endl
-         << "width: "  << pcs[i].monitor.screenSize.width << endl
-         << "resolution: " << pcs[i].monitor.resolution << endl
-         << "serial num: " << pcs[i].monitor.base.serialNum << endl;
+         << "height: " << pcs.at(i).monitor.screenSize.height << endl
+         << "width: "  << pcs.at(i).monitor.screenSize.width << endl
+         << "resolution: " << pcs.at(i).monitor.resolution << endl
+         << "serial num: " << pcs.at(i).monitor.base.serialNum << endl;
 
     cout << "Mouse--------------------------------------\n"
-         << "serial num: " << pcs[i].mouse.base.serialNum << endl;
-
+         << "serial num: " << pcs.at(i).mouse.base.serialNum << endl;
 }
 
 int main () {
-    PC pcs[NUM_OF_PC];
-    bool isFound = false;
+    PC pc1 = {{"c001","model1","acer","c1"}, {{"keyboard001"}}, {{"mouse001"}}, {{"cpu001"},{{{"core1-001"}}, {{"core1-002"}}, {{"core1-003"}}, {{"core1-004"}} }, 4, 300},{{"graphics001","model1","GForce"}}, {{"Hdd001"}, 125, "Sata"}, {{"monitor001"}, 1000, {1000, 800}}};
+    PC pc2 = {{"c002","model2","DELL","c2"}, {{"keyboard002"}}, {{"mouse002"}}, {{"cpu002"},{{{"core2-001"}}, {{"core2-002"}}, {{"core2-003"}}, {{"core2-004"}} }, 4, 300},{{"graphics002","model2","GForce"}}, {{"Hdd002"}, 125, "Sata"}, {{"monitor002"}, 1000, {1000, 800}}};
+    PC pc3 = {{"c003","model3","HP","c3"  }, {{"keyboard003"}}, {{"mouse003"}}, {{"cpu003"},{{{"core3-001"}}, {{"core3-002"}}, {{"core3-003"}}, {{"core3-004"}} }, 4, 300},{{"graphics003","model3","GForce"}}, {{"Hdd003"}, 125, "Sata"}, {{"monitor003"}, 1000, {1000, 800}}};
+    PC pc4 = {{"c004","model4","ASUS","c4"}, {{"keyboard004"}}, {{"mouse004"}}, {{"cpu004"},{{{"core4-001"}}, {{"core4-002"}}, {{"core4-003"}}, {{"core4-004"}} }, 4, 300},{{"graphics004","model4","GForce"}}, {{"Hdd004"}, 125, "Sata"}, {{"monitor004"}, 1000, {1000, 800}}};
 
-    pcs[0] = {{"c001","model1","acer","c1"}, {{"keyboard001"}}, {{"mouse001"}}, {{"cpu001"},{{{"core1-001"}}, {{"core1-002"}}, {{"core1-003"}}, {{"core1-004"}} }, 4, 300},{{"graphics001","model1","GForce"}}, {{"Hdd001"}, 125, "Sata"}, {{"monitor001"}, 1000, {1000, 800}}};
-    pcs[1] = {{"c002","model2","DELL","c2"}, {{"keyboard002"}}, {{"mouse002"}}, {{"cpu002"},{{{"core2-001"}}, {{"core2-002"}}, {{"core2-003"}}, {{"core2-004"}} }, 4, 300},{{"graphics002","model2","GForce"}}, {{"Hdd002"}, 125, "Sata"}, {{"monitor002"}, 1000, {1000, 800}}};
-    pcs[2] = {{"c003","model3","HP","c3"  }, {{"keyboard003"}}, {{"mouse003"}}, {{"cpu003"},{{{"core3-001"}}, {{"core3-002"}}, {{"core3-003"}}, {{"core3-004"}} }, 4, 300},{{"graphics003","model3","GForce"}}, {{"Hdd003"}, 125, "Sata"}, {{"monitor003"}, 1000, {1000, 800}}};
-    pcs[3] = {{"c004","model4","ASUS","c4"}, {{"keyboard004"}}, {{"mouse004"}}, {{"cpu004"},{{{"core4-001"}}, {{"core4-002"}}, {{"core4-003"}}, {{"core4-004"}} }, 4, 300},{{"graphics004","model4","GForce"}}, {{"Hdd004"}, 125, "Sata"}, {{"monitor004"}, 1000, {1000, 800}}};
+    vector<PC> pcs = {pc1, pc2, pc3, pc4};
 
-    int num = chooseMenu();
-    string keyword = getInput();
-    int resultIndex = searchPC(num,keyword,pcs,isFound);
-    cout << resultIndex << endl;
-    displayResult(resultIndex,pcs);
+    int num = getMenuNumberFromUser();
+    string keyword = getKeywordFromUser();
+    int pcIndex = searchPC(num, keyword, pcs);
+    if (pcIndex >= 0 && pcIndex <= pcs.size()-1) {
+        displayResult(pcIndex, pcs);
+    } else {
+        cout << "No matching PC found." << endl;
+    }
 
     return 0;
 }
